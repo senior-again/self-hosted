5bf59f8b4ac8cbff2c3efa7d9a04fa995ed81665
minsang-alt <mazinger97@naver.com>
2024-02-19 16:33:47 +0900

    initial commit

---
+++ src/main/java/com/hosted/selfhosted/SelfHostedApplication.java
@@ -0,0 +1,13 @@
+package com.hosted.selfhosted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHostedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHostedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hosted/selfhosted/SelfHostedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosted.selfhosted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHostedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
