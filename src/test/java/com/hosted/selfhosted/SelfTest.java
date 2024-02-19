299d38801551cbb89f119596772c64a277aee545
minsang-alt <mazinger97@naver.com>
2024-02-19 16:55:21 +0900

    test: success test

--- src/test/java/com/hosted/selfhosted/SelfTest.java
+++ src/test/java/com/hosted/selfhosted/SelfTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 
 public class SelfTest {
 
-    @Test
+    /*@Test
     void failure_Test(){
         //실패하는 테스트
         throw new RuntimeException();
+    }*/
+
+    @Test
+    void success_test(){
+        //성공하는 테스트
     }
 }
